6c6e9c6c4011345533cb12595e4dcfb83f629b58
KerenMedina <47213202+KerenMedina@users.noreply.github.com>
2025-04-04 17:23:44 +0200

    add css

--- Ejercicio2/index.php
+++ Ejercicio2/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reserva tu Cita</title>
+    <link rel="stylesheet" href="css.css">
 
 </head>
 <body>
@@ -11,32 +12,32 @@
     <form action="">
         <label for="name">Nombre:</label>
         <input type="text" id="name" name="name" required><br><br>
-        <div id="name_error" style="color: red;"></div>
+        <div id="name_error"class="error" ></div>
 
         <label for="dni">DNI:</label>
         <input type="text" id="dni" name="dni" required "><br><br>
-        <div id="dni_error" style="color: red;"></div>
+        <div id="dni_error" class="error" ></div>
 
         <label for="phone">Teléfono:</label>
         <input type="text" id="phone" name="phone" required><br><br>
-        <div id="phone_error" style="color: red;"></div>
+        <div id="phone_error" class="error" ></div>
 
         <label for="email">Email:</label>
         <input type="email" id="email" name="email" required><br><br>
-        <div id="email_error" style="color: red;"></div>
+        <div id="email_error" class="error" ></div>
 
         <label for="appointment_type">Tipo de cita:</label>
         <select id="appointment_type" name="appointment_type" disabled required>
             <option selected value="Primera consulta">Primera consulta</option>
             <option value="Revisión">Revisión</option>
         </select><br><br>
-        <div id="appointment_type_error" style="color: yellow;"></div>
+        <div id="appointment_type_error" class="error" style="color: yellow;"></div>
 
-
+        <div id="result" class="success" ></div>
         <button type="button" id="submit">Pedir Cita</button>
     </form>
 
-    <div id="result" style=" color: green;"></div>
+   
   
 </body>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
